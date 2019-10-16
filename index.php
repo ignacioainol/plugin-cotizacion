91e5f95aea3e23cad3d04ed76001588263586698
Edgardo Ainol Rivera <edgardo.ainol@CRECIC.CL>
2019-10-16 17:45:10 -0300

    adding js

--- index.php
+++ index.php
@@ -10,54 +10,44 @@ License: GPLv2 or later
 Text Domain: Cotizaciones Crecicyrax
 */
 
-global $jal_db_version;
-$jal_db_version = '1.0';
+//Creando tablas en la base de datos
+require 'install/Jal_install.php';
+$jai_install = new Jal_install;
 
-function jal_install() {
-	global $wpdb;
-	global $jal_db_version;
 
-	$table_cotizaciones          = $wpdb->prefix . 'cotizaciones_crecic';
-	$table_detalles_cotizaciones = $wpdb->prefix . 'detalles_cotizaciones_crecic';
-	
-	$charset_collate = $wpdb->get_charset_collate();
-
-	$sql = "
-        CREATE TABLE $table_cotizaciones (
-		id mediumint(9) NOT NULL AUTO_INCREMENT,
-		created_at datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
-		email VARCHAR(100) NOT NULL,
-		PRIMARY KEY  (id)
-    ) $charset_collate;";
-    
-    $sqlDetalle = "
-        CREATE TABLE $table_detalles_cotizaciones(
-        id mediumint(9) NOT NULL AUTO_INCREMENT,
-        product_name VARCHAR(100),
-        id_cotizacion mediumint(9) NOT NULL,
-        FOREIGN KEY (id_cotizacion) REFERENCES $table_cotizaciones(id),
-        PRIMARY KEY (id)
-        ) $charset_collate;";
-
-	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-    dbDelta([$sql, $sqlDetalle]);
-
-	add_option( 'jal_db_version', $jal_db_version );
+function cs_additional_button() {
+     echo '<button type="submit" style="background:orangered;" class="button alt">Añadir a cotizacion</button>';
 }
 
-/*function jal_install_data() {
-	global $wpdb;
-	
-	$table_cotizaciones = $wpdb->prefix . 'cotizaciones_crecic';
-	
-	$wpdb->insert( 
-		$table_cotizaciones, 
-		array( 
-			'created_at' => date("Y-m-d H:i:s"), 
-			'email' => 'ignacio.ainolrivera@gmail.com', 
-		) 
-	);
-}*/
+function muestra_porcentaje_descuento(){
+    $postId = get_the_ID();
+    echo '<button data-idPost="'.$postId.'" type="submit" style="background:orangered;" class="button alt addCotizacion">Añadir a cotizacion</button>';
+}
 
-register_activation_hook( __FILE__, 'jal_install' );
-// register_activation_hook( __FILE__, 'jal_install_data' );
+add_action('woocommerce_after_add_to_cart_button', 'cs_additional_button');
+//this
+add_action('woocommerce_after_shop_loop_item', 'muestra_porcentaje_descuento');
+add_action( 'wp_footer', 'my_action_javascript' ); // Write our JS below here
+
+function my_action_javascript() { ?>
+	<script type="text/javascript" >
+	jQuery(document).ready(function($) {
+        $('.addCotizacion').on('click',function(){
+            $.ajax({
+                url: 'http://'+window.location.hostname+'/wp-admin/admin-ajax.php',
+                type: 'POST',
+                data: {
+                    test: 'algo'
+                },
+                success: function(response){
+                    alert(response);
+                }
+                .error: function(msg){
+                    console.log(msg);
+                }
+            });
+        })
+	});
+	</script> <?php
+}
+?>
